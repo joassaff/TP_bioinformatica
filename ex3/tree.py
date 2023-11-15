e0f680e4de36fc41535628637c95881b0579c71b
joassaff <jassaff@itba.edu.ar>
2023-11-15 19:24:45 -0300

    arbol filogenetico
    arbol filogenetico codigo e imagen

--- ex3/tree.py
+++ ex3/tree.py
@@ -1,7 +1,6 @@
 from ete3 import Tree, TreeStyle
 from Bio import Phylo
 
-# Cargar el árbol desde el archivo
 tree = Tree("output_tree.nwk")
 
 ts = TreeStyle()
@@ -9,29 +8,23 @@ ts.show_leaf_name = True
 ts.show_branch_length = True
 ts.show_branch_support = True
 
-# Dibujar el árbol y mostrarlo
-tree.render("tree.png", w=500, units="mm", tree_style=ts)  # Ajustar el valor de w según tus preferencias
+tree.render("tree.png", w=500, units="mm", tree_style=ts)  
 tree.show()
 
 tree2 = Phylo.read("output_tree.nwk", "newick")
 
-# Define el nodo objetivo
 target_node_name = "HomoSap"
 
-# Encuentra el nodo objetivo en el árbol
 target_node = next((node for node in tree2.find_clades() if node.name == target_node_name), None)
 
 for clade in tree2.find_clades():
     print(clade.name)
-# Verifica si el nodo objetivo se encontró
 if target_node is None:
     print(f"No se encontró el nodo con el nombre {target_node_name}")
 else:
-    # Inicializa la distancia mínima y el nodo correspondiente
-    min_distance = float('inf')  # Inicializa con infinito para garantizar que la primera distancia sea menor
+    min_distance = float('inf')
     closest_node = None
 
-    # Itera sobre todos los nodos del árbol y calcula la distancia al nodo objetivo
     for clade in tree2.find_clades():
         print("hola")
         if clade != target_node and clade.name is not None:
@@ -40,7 +33,6 @@ else:
                 min_distance = distance_to_target
                 closest_node = clade
 
-    # Imprime la distancia mínima y el nodo más cercano
     print(f"Distancia mínima a {target_node_name}: {min_distance}")
     print(f"Nodo más cercano: {closest_node.name if closest_node else 'Ninguno'}")
 
